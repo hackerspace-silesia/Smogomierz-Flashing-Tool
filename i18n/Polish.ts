b86b0d544072c58151eff8efb35c8ef3b6915dd8
bfaliszek <blazejfaliszek@gmail.com>
2019-01-11 13:00:53 +0100

    translation update

--- i18n/Polish.ts
+++ i18n/Polish.ts
@@ -13,7 +13,7 @@
         <translation>Inne...</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="197"/>
+        <location filename="../smogomierz-tool.py" line="241"/>
         <source>No device selected.</source>
         <translation>Nie wybrano urządzenia.</translation>
     </message>
@@ -23,29 +23,34 @@
         <translation>Nie wybrano wersji.</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="226"/>
+        <location filename="../gui/mainwindow.py" line="235"/>
         <source>Board:</source>
-        <translation>Płytka:</translation>
+        <translation type="obsolete">Płytka:</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="223"/>
+        <location filename="../gui/mainwindow.py" line="231"/>
         <source>Firmware version:</source>
-        <translation>Wersja oprogramowania:</translation>
+        <translation type="obsolete">Wersja oprogramowania:</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="224"/>
-        <source>Upload</source>
-        <translation>Wgraj</translation>
+        <location filename="../gui/mainwindow.py" line="232"/>
+        <source>Upload Firmware</source>
+        <translation type="obsolete">Wgraj Oprogramowanie</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="225"/>
+        <location filename="../gui/mainwindow.py" line="233"/>
+        <source>Erase Flash</source>
+        <translation type="obsolete">Wyzeruj ESP8266</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="234"/>
         <source>Expert mode</source>
-        <translation>Tryb eksperta</translation>
+        <translation type="obsolete">Tryb eksperta</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="227"/>
+        <location filename="../gui/mainwindow.py" line="236"/>
         <source>Baudrate:</source>
-        <translation>Prędkość portu:</translation>
+        <translation type="obsolete">Prędkość portu:</translation>
     </message>
     <message>
         <location filename="../smogomierz-tool.py" line="221"/>
@@ -58,27 +63,27 @@
         <translation>Praca w toku...</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="253"/>
+        <location filename="../smogomierz-tool.py" line="273"/>
         <source>Downloading...</source>
         <translation>Pobieranie...</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="304"/>
+        <location filename="../smogomierz-tool.py" line="324"/>
         <source>Connecting...</source>
         <translation>Łączenie...</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="308"/>
+        <location filename="../smogomierz-tool.py" line="328"/>
         <source>Connected. Chip type: {chip_type}</source>
         <translation>Połączono. Typ układu: {chip_type}</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="286"/>
+        <location filename="../smogomierz-tool.py" line="306"/>
         <source>Writing at 0x{address:08x}...</source>
         <translation>Zapisywanie pod adresem 0x{address:08x}...</translation>
     </message>
     <message>
-        <location filename="../smogomierz-tool.py" line="297"/>
+        <location filename="../smogomierz-tool.py" line="317"/>
         <source>Finished in {time:.2f} seconds. Sensor ID: {sensor_id}</source>
         <translation>Zakończono w {time:.2f} sekundy. ID sensora: {sensor_id}</translation>
     </message>
@@ -98,69 +103,79 @@
         <translation>Czy zainstalowałeś &lt;a href=&quot;{drivers_url}&quot;&gt;sterowniki&lt;/a&gt;?</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="228"/>
-        <source>Flashing</source>
-        <translation>Programowanie</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="229"/>
-        <source>Double-click to open configuration page.</source>
-        <translation>Naciśnij dwukrotnie aby otworzyć stronę konfiguracji.</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="230"/>
-        <source>Sensors detected in local network:</source>
-        <translation>Sensory wykryte w sieci lokalnej:</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="231"/>
-        <source>Refresh</source>
-        <translation>Odśwież</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="232"/>
-        <source>Discovery</source>
-        <translation>Wykrywanie</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="233"/>
-        <source>In case of sensor issues, Serial Monitor can be used to review logs sent by the sensor over USB cable.</source>
-        <translation>W przypadku problemów z sensorem, Serial Monitor pozwala na przejrzenie stanu urządzenia przez kabel USB.</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="234"/>
-        <source>Connect</source>
-        <translation>Połącz</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.py" line="235"/>
-        <source>Serial Monitor</source>
-        <translation>Serial Monitor</translation>
-    </message>
-    <message>
         <location filename="../gui/mainwindow.py" line="237"/>
+        <source>Flashing</source>
+        <translation type="obsolete">Programowanie</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="238"/>
+        <source>Double-click to open configuration page.</source>
+        <translation type="obsolete">Naciśnij dwukrotnie aby otworzyć stronę konfiguracji.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="239"/>
+        <source>Sensors detected in local network:</source>
+        <translation type="obsolete">Sensory wykryte w sieci lokalnej:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="240"/>
+        <source>Refresh</source>
+        <translation type="obsolete">Odśwież</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="241"/>
+        <source>Discovery</source>
+        <translation type="obsolete">Wykrywanie</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="242"/>
+        <source>In case of sensor issues, Serial Monitor can be used to review logs sent by the sensor over USB cable.</source>
+        <translation type="obsolete">W przypadku problemów z sensorem, Serial Monitor pozwala na przejrzenie stanu urządzenia przez kabel USB.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="243"/>
+        <source>Connect</source>
+        <translation type="obsolete">Połącz</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="244"/>
+        <source>Serial Monitor</source>
+        <translation type="obsolete">Serial Monitor</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="246"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Developed by &lt;a href=&quot;https://hs-silesia.pl/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Hackerspace Silesia&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Based on &lt;a href=&quot;https://github.com/opendata-stuttgart/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;luftdaten.info flashing tool&lt;/span&gt;&lt;/a&gt; project..&lt;/p&gt;&lt;p&gt;This software is released under the terms of MIT license. No warranty is provided.&lt;/p&gt;&lt;p&gt;For newest release see: &lt;a href=&quot;https://github.com/hackerspace-silesia/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/hackerspace-silesia/Smogomierz-Flashing-Tool/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wykonane przez &lt;a href=&quot;https://hs-silesia.pl/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Hackerspace Silesia&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Program ten bazuje na projekcie &lt;a href=&quot;https://github.com/opendata-stuttgart/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;luftdaten.info flashing tool&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Ten program wypuszczony jest na warunkach licencji MIT. Brak gwarancji.&lt;/p&gt;&lt;p&gt;Najnowszą wersję znajdziesz na: &lt;a href=&quot;https://github.com/hackerspace-silesia/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/hackerspace-silesia/Smogomierz-Flashing-Tool/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="238"/>
+        <location filename="../gui/mainwindow.py" line="247"/>
         <source>About</source>
-        <translation>O programie</translation>
+        <translation type="obsolete">O programie</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="222"/>
+        <location filename="../gui/mainwindow.py" line="230"/>
         <source>Smogomierz Flashing Tool (v{version})</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Smogomierz Flashing Tool (v{version})</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="236"/>
+        <location filename="../gui/mainwindow.py" line="245"/>
         <source>&lt;b&gt;Smogomierz Flashing Tool&lt;/b&gt;&lt;br/&gt;Build {build_id}</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">&lt;b&gt;Smogomierz Flashing Tool&lt;/b&gt;&lt;br/&gt;Build {build_id}</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.py" line="237"/>
+        <location filename="../gui/mainwindow.py" line="246"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Developed by &lt;a href=&quot;https://hs-silesia.pl/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Hackerspace Silesia&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Based on &lt;a href=&quot;https://github.com/opendata-stuttgart/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;luftdaten.info flashing tool&lt;/span&gt;&lt;/a&gt; project.&lt;/p&gt;&lt;p&gt;This software is released under the terms of MIT license. No warranty is provided.&lt;/p&gt;&lt;p&gt;For newest release see: &lt;a href=&quot;https://github.com/hackerspace-silesia/Smogomierz-Flashing-Tool/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/hackerspace-silesia/Smogomierz-Flashing-Tool/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wykonane przez &lt;a href=&quot;https://hs-silesia.pl/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Hackerspace Silesia&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Program ten bazuje na projekcie &lt;a href=&quot;https://github.com/opendata-stuttgart/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;luftdaten.info flashing tool&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Ten program wypuszczony jest na warunkach licencji MIT. Brak gwarancji.&lt;/p&gt;&lt;p&gt;Najnowszą wersję znajdziesz na: &lt;a href=&quot;https://github.com/hackerspace-silesia/airrohr-firmware-flasher&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/hackerspace-silesia/Smogomierz-Flashing-Tool/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../smogomierz-tool.py" line="245"/>
+        <source>Erasing in progress...</source>
+        <translation type="unfinished">Kasowanie w toku...</translation>
+    </message>
+    <message>
+        <location filename="../smogomierz-tool.py" line="338"/>
+        <source>Erasing complet!</source>
+        <translation type="unfinished">Kasowanie zakończone!</translation>
     </message>
 </context>
 </TS>
